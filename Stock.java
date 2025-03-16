683eb1ec6d4e28e1d890f0f6008902670c782ea4
amira-rozale <126015403+amira-rozale@users.noreply.github.com>
2025-03-16 12:16:54 +0100

    Update Stock.java

--- Stock.java
+++ Stock.java
@@ -1,13 +1,1 @@
-package RMIstock;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.List;
 
-public interface Stock extends Remote {
-    List<Produit> getListeProduits() throws RemoteException;
-    void ajouterProduit(Produit produit) throws RemoteException;
-    void supprimerProduit(Produit produit) throws RemoteException;
-    //
-    int getNombreProduits() throws RemoteException;
-    Produit chercherProduit(String nomProduit) throws RemoteException;
-    }
